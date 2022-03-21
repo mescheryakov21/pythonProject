ec2d84d307dfb8fa003ca1c95a41c83b4bdc333a
mescheryakov21 <volody1987@mail.ru>
2022-03-21 11:11:07 +0300

    done resheto Eratosphena

--- Test.py
+++ Test.py
@@ -1,4 +1,14 @@
-n = int(input("Введите простое число до какого числа простые: "))
-start_list =[i for i in range(2, n + 1)]
-
-print(start_list)
+'''
+Решето Эратосфена
+'''
+n = int(input("Введите простое число до какого числа простые включительно: "))
+start_list =[i for i in range(n+1)]
+start_list[1] = 0
+for i in range(2, n+1):
+    if start_list[i] != 0:
+        j = i + i
+        while j <= n:
+            start_list[j] = 0
+            j += i
+result = [i for i in start_list if i != 0]
+print(result)
