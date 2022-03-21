3c0de1b54333ab8147525165e7f0f0717ec6c7fb
mescheryakov21 <volody1987@mail.ru>
2022-03-21 11:37:13 +0300

    modif task

--- less_task_2a.py
+++ less_task_2a.py
@@ -7,15 +7,23 @@
 '''
 
 def test_func(func):
-    lst = [2, 3, 5, 7, 11]
+    lst = [2, 3, 5, 7, 11, 13]
     for i in range(len(lst)):
-        assert lst[i] == func(i+1)
+        assert lst[i] == func(i)
 
 def prost_1(n):
-    len_p = 0
+    if i != 0:
+       start_list = [i for i in range(n)]
+       start_list[1] = 0
+       for i in range(2, n + 1):
+            if start_list[i] != 0:
+                j = i + i
+                while j <= n:
+                    start_list[j] = 0
+                    j += i
+       result = [i for i in start_list if i != 0]
+       return result[n-1]
 
-    if n != 0:
-        while n > len_p:
+test_func(prost_1)
 
 
-    return 2
