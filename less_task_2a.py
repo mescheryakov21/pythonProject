f1eb733f09cff015adfc46eaffdeb2d048b04e16
mescheryakov21 <volody1987@mail.ru>
2022-03-22 19:52:02 +0300

    modific task_2

--- less_task_2a.py
+++ less_task_2a.py
@@ -6,24 +6,18 @@
 Классический Алгоритм
 '''
 
-def test_func(func):
-    lst = [2, 3, 5, 7, 11, 13]
-    for i in range(len(lst)):
-        assert lst[i] == func(i)
-
 def prost_1(n):
-    if i != 0:
-       start_list = [i for i in range(n)]
-       start_list[1] = 0
-       for i in range(2, n + 1):
-            if start_list[i] != 0:
-                j = i + i
-                while j <= n:
-                    start_list[j] = 0
-                    j += i
-       result = [i for i in start_list if i != 0]
-       return result[n-1]
-
-test_func(prost_1)
+   start_list = [i for i in range(n * 10 +1)]
+   start_list[1] = 0
+   for i in range(2,len(start_list)):
+        if start_list[i] != 0:
+            j = i + i
+            while j <= n * 10:
+                start_list[j] = 0
+                j += i
+   result = [i for i in start_list if i != 0]
+   return result[n-1]
+poryadok = int(input("Введите место простого числа: "))
+print(prost_1(poryadok))
 
 
