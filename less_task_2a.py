7c0aec4efbf132afeee4686716345cb70a49bdcf
mescheryakov21 <volody1987@mail.ru>
2022-03-22 20:44:17 +0300

    done task_2a

--- less_task_2a.py
+++ less_task_2a.py
@@ -7,17 +7,23 @@
 '''
 
 def prost_1(n):
-   start_list = [i for i in range(n * 10 +1)]
+   if n == 0:
+       return 0
+   k = pow(n, 2) + 2
+   start_list = [i for i in range(k)]
    start_list[1] = 0
-   for i in range(2,len(start_list)):
+   for i in range(2, k):
         if start_list[i] != 0:
             j = i + i
-            while j <= n * 10:
+            while j < k:
                 start_list[j] = 0
                 j += i
    result = [i for i in start_list if i != 0]
    return result[n-1]
-poryadok = int(input("Введите место простого числа: "))
-print(prost_1(poryadok))
+
+poryadok = 1
+while poryadok != 0:
+    poryadok = int(input("Введите место простого числа: "))
+    print(prost_1(poryadok))
 
 
