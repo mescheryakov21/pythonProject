a78cd6ae56a1d72ae1ccd06f6197b7c5d79b342c
mescheryakov21 <volody1987@mail.ru>
2022-03-22 21:13:50 +0300

    modificed 2aa add

--- less_task_2aa.py
+++ less_task_2aa.py
@@ -13,22 +13,26 @@ def prost_1(n):
    k = 1
    if result[n-1] != None:
        return result[n - 1]
-   while n > len(result)
-   start_list = [i for i in range(k)]
-   start_list[1] = 0
-   for i in range(2, k):
-        if start_list[i] != 0:
-            j = i + i
-            while j < k:
-                start_list[j] = 0
-                j += i
-   result = [i for i in start_list if i != 0]
+   while n > len(result):
+       k = n * k
+       start_list = [i for i in range(k)]
+       start_list[1] = 0
+       for i in range(2, k):
+            if start_list[i] != 0:
+                j = i + i
+                while j < k:
+                    start_list[j] = 0
+                    j += i
+       k += 1
+       result_1 = [i for i in start_list if i != 0]
+       for i in range(len(result), len(result_1)):
+           result.append(result_1[i])
    return result[n-1]
 
-# poryadok = 1
-# while poryadok != 0:
-#     poryadok = int(input("Введите место простого числа: "))
-#     print(prost_1(poryadok))
+poryadok = 1
+while poryadok != 0:
+    poryadok = int(input("Введите место простого числа: "))
+    print(prost_1(poryadok))
 
 # "less_task_2a.prost_1(10)"
 # 1000 loops, best of 5: 57.9 usec per loop
