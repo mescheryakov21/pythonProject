05ad0eb76f3a00eeea21914748722487aeb9c675
mescheryakov21 <volody1987@mail.ru>
2022-03-27 19:53:44 +0300

    done task 3

--- lesson_5_defaultdict.py
+++ lesson_5_defaultdict.py
@@ -4,6 +4,15 @@ s = 'skflgj;asjpwejfafsfFWAGFVAGSFGGasfdg'
 a = defaultdict(int)
 for i in s:
     a[i] += 1
-print(a)
+# print(a)
 c = Counter(s)
-print(c)
+# print(c)
+list_1 = [('cat', 2), ('dog', 1), ('cat', 2)]
+b = defaultdict(list)
+c = defaultdict(set)
+for i, j in list_1:
+    b[i].append(j)
+for i, j in list_1:
+    c[i].add(j)
+print(b, c, sep="\n")
+
