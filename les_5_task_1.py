6a139cc677e3475f7d8753b2fe9ead1133021955
mescheryakov21 <volody1987@mail.ru>
2022-03-29 22:44:46 +0300

    done task_1

--- les_5_task_1.py
+++ les_5_task_1.py
@@ -10,11 +10,19 @@ full_dict = ChainMap()
 sum_profit = 0
 while k <= n:
     name_firma = input(f'Введите наименование {k}го предприятия: ')
-    profit = int(input(f'Введите прибыль за IV квартала {k}го предприятия: '))
+    profit = float(input(f'Введите прибыль за IV квартала {k}го предприятия: '))
     dict_spam = {'name': name_firma, 'profit': profit }
     full_dict = full_dict.new_child(dict_spam)
     k += 1
     sum_profit += profit
 arg_profit = sum_profit / n
-# if profit < arg_profit:
-print(full_dict, sum_profit, arg_profit )
+a = list()
+b = list()
+
+for i in range(n):
+    c = full_dict.maps[i]['profit']
+    if c > arg_profit:
+        a.append(full_dict.maps[i]['name'])
+    else:
+        b.append(full_dict.maps[i]['name'])
+print(f'Предприятия с большей прибылью, чем среднее {arg_profit}: {a} \nПредприятия с меньшей прибылью чем среднее {arg_profit}: {b}')
