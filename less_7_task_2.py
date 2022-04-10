dc3b8f697cad9fe64c5a509eae9708de0721cad9
mescheryakov21 <volody1987@mail.ru>
2022-04-10 17:03:44 +0300

    done task_2

--- less_7_task_2.py
+++ less_7_task_2.py
@@ -5,18 +5,28 @@
 
 import random
 
-def slit_arr(array):
-    '''
-    Метод сортировки Слияние
-    :param array:  - неотсортированный массив
-    :return: - отсортированный массив
-    '''
-    if len(array) == 1:
-        return array
+def slit_arr(arr):
+    if len(arr) == 1:
+        return arr
+    a = slit_arr(arr[:len(arr) // 2])
+    b = slit_arr(arr[len(arr) // 2:])
+    k = 0
+    l = 0
+    c = list()
+    while k < len(a) and l < len(b):
+        if a[k] < b[l]:
+            c.append(a[k])
+            k += 1
+        else:
+            c.append(b[l])
+            l += 1
+    if len(a) > k:
+        c.extend(a[k:])
+    elif len(b) > l:
+        c.extend(b[l:])
+    return c
 
-
-
-size = 20
+size = 100
 array_ramd = [random.randint(0, 49) for i in range(size)]
 print(array_ramd)
-slit_arr(array_ramd)
+print(slit_arr(array_ramd))
